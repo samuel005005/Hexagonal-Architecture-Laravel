944af76180873b0ba2521acb7bb73dd7b1c4b2c1
Samuel Paez <spaez@viva.com.do>
2023-04-16 22:54:19 -0400

    first commit

---
+++ src/User/Application/UserLoginUseCase.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\User\Application;
+
+final class UserLoginUseCase
+{
+    public function execute(): void
+    {
+
+    }
+}
---
+++ src/User/Application/UserSearchUseCase.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Src\User\Application;
+
+final class UserSearchUseCase
+{
+    public function  execute(): void
+    {
+
+    }
+}
